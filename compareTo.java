222a1367716859265fb29b33771a3602d419c237
yakup <lvashy86@gmail.com>
2023-02-10 21:58:25 +0300

    03

--- compareTo.java
+++ compareTo.java
@@ -14,3 +14,14 @@ public class compareTo {
 			B.substring(0,1).toUpperCase()+B.substring(1,B.length()));
 	}
 }
+
+// Input Format
+
+// The first line contains a string . The second line contains another string . The strings are comprised of only lowercase English letters.
+
+// Output Format
+
+// There are three lines of output:
+// For the first line, sum the lengths of  and .
+// For the second line, write Yes if  is lexicographically greater than  otherwise print No instead.
+// For the third line, capitalize the first letter in both  and  and print them on a single line, separated by a space.
